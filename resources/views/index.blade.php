ddf6726aaa618b9697e31173639e14c180c63bd1
MaiTranAnhTuan <maituan5540@gmail.com>
2024-03-24 18:00:04 +0700

    Lien ket cac trang va chinh lai css

---
+++ resources/views/index.blade.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<!-- Website template by freewebsitetemplates.com -->
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Frozen Yogurt Shop</title>
+	<link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}" type="text/css">
+	<link rel="stylesheet" href="{{ asset('assets/css/mobile.css') }}" type="text/css">
+	<script src="{{ asset('assets/js/mobile.css') }}" type="text/javascript"></script>
+</head>
+<body>
+	<div id="page">
+		<div id="header">
+			<div>
+				<a href="index" class="logo"><img src="/images/logo.png" alt=""></a>
+				<ul id="navigation">
+					<li class="selected">
+						<a href="index">Home</a>
+					</li>
+					<li class="menu">
+						<a href="about">About</a>
+						<ul class="primary">
+							<li>
+								<a href="product">Product</a>
+							</li>
+						</ul>
+					</li>
+					<li class="menu">
+						<a href="blog">Blog</a>
+						<ul class="secondary">
+							<li>
+								<a href="singlepost">Single post</a>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="contact">Contact</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div id="body" class="home">
+			<div class="header">
+				<img src="/images/bg-home.jpg" alt="">
+				<div>
+					<a href="product">Freeze Delight</a>
+				</div>
+			</div>
+			<div class="body">
+				<div>
+					<div>
+						<h1>NEW PRODUCT</h1>
+						<h2>The Twist of Healthy Yogurt</h2>
+						<p>This website template has been designed by freewebsitetemplates.com for you, for free. You can replace all this text with your own text.</p>
+					</div>
+					<img src="/images/yogurt.jpg" alt="">
+				</div>
+			</div>
+			<div class="footer">
+				<div>
+					<ul>
+						<li>
+							<a href="product" class="product"></a>
+							<h1>PRODUCTS</h1>
+						</li>
+						<li>
+							<a href="about" class="about"></a>
+							<h1>OUR STORY</h1>
+						</li>
+						<li>
+							<a href="product" class="flavor"></a>
+							<h1>FLAVORS</h1>
+						</li>
+						<li>
+							<a href="contact" class="contact"></a>
+							<h1>OUR LOCATION</h1>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div id="footer">
+			<div>
+				<div class="connect">
+					<a href="http://freewebsitetemplates.com/go/facebook/" class="facebook">facebook</a>
+					<a href="http://freewebsitetemplates.com/go/twitter/" class="twitter">twitter</a>
+					<a href="http://freewebsitetemplates.com/go/googleplus/" class="googleplus">googleplus</a>
+					<a href="http://pinterest.com/fwtemplates/" class="pinterest">pinterest</a>
+				</div>
+				<p>&copy; 2023 Freeeze. All Rights Reserved.</p>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
