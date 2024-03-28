aefe6870eaca077e9f9ce65911603559919d2bfb
Mnh_tr <minhtri0428@gmail.com>
2024-03-28 10:30:08 +0700

    tri_add_value_productTable

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Seeders;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,5 +15,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::table('product')->insert([
+            'tenSP'=> "san pham 1",
+            'soLuong'=> 50,
+            'moTa'=> "san pham khong phai la thuoc de thay the thuoc chua benh",
+            'anhSP'=> "khong co",
+            'maDM'=> "2",
+            'giamGia'=> "20%",
+            'luotXem'=> "10000000",
+            'donGia'=> "23k"
+        ]);
     }
 }
