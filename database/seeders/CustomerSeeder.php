9eb5331630b244571975aadf20d8b328257388ea
NguyenThanhTai1604 <nthanhtai1604@gmail.com>
2024-04-10 18:18:08 +0700

    abc

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -15,37 +15,6 @@ class CustomerSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('customers')->insert([
-            [
-                'customer_name' => "Nguyen Van A",
-                'customer_address' => "Nha Trang",
-                'customer_phone' => "0123456789",
-                'customer_email' => "nguyenvanA@gmail.com"
-            ],
-            [
-                'customer_name' => "Nguyen Van B",
-                'customer_address' => "Da Lat",
-                'customer_phone' => "0123459876",
-                'customer_email' => "nguyenvanB@gmail.com"
-            ],
-            [
-                'customer_name' => "Nguyen Van C",
-                'customer_address' => "Ha Noi",
-                'customer_phone' => "0123756489",
-                'customer_email' => "nguyenvanC@gmail.com"
-            ],
-            [
-                'customer_name' => "Nguyen Van D",
-                'customer_address' => "Ho Chi Minh",
-                'customer_phone' => "0124356789",
-                'customer_email' => "nguyenvanD@gmail.com"
-            ],
-            [
-                'customer_name' => "Nguyen Van E",
-                'customer_address' => "Quang Nam",
-                'customer_phone' => "0123486579",
-                'customer_email' => "nguyenvanE@gmail.com"
-            ],
-        ]);
+        DB::table('customers')->insert([]);
     }
 }
