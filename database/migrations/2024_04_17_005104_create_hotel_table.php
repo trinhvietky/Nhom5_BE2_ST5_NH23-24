f416c4d1f91827e8a7fab9b857d96aa1d0a2f2ac
NguyenThanhTai1604 <nthanhtai1604@gmail.com>
2024-04-17 08:11:58 +0700

    abc

--- database/migrations/2024_04_17_005104_create_hotel_table.php
+++ database/migrations/2024_04_17_005104_create_hotel_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
+        Schema::create('hotel', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -22,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('customers');
+        Schema::dropIfExists('hotel');
     }
 };
