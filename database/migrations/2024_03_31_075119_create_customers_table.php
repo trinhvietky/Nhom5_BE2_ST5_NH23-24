db49be92216bf2be9771251453bbdd4e427a2410
NguyenThanhTai1604 <nthanhtai1604@gmail.com>
2024-04-17 08:12:42 +0700

    abc

--- database/migrations/2024_03_31_075119_create_customers_table.php
+++ database/migrations/2024_03_31_075119_create_customers_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('customer_id');
+            $table->string('customer_name');
+            $table->string('customer_address');
+            $table->integer('customer_phone')->unsigned();
+            $table->string('customer_email');
+            $table->timestamps();
+        });
     }
 
     /**
