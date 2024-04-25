e0909ede914cfece6d36b68b3a53e5af8ac3c2ee
maitrananhtuan <maituan5540@gmail.com>
2024-04-25 07:33:46 +0700

    Sua lai Auth

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -42,10 +42,10 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
-        // event(new Registered($user));
+        event(new Registered($user));
 
         // Auth::login($user);
 
-        return redirect(RouteServiceProvider::HOME);
+        return redirect(RouteServiceProvider::HOME2);
     }
 }
