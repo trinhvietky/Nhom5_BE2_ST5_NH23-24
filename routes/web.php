f63f77b410809ab6665d114a0c6cc65084fd0ee9
maitrananhtuan <maituan5540@gmail.com>
2024-04-25 07:11:56 +0700

    Sua lai duog lien ket Login va Register

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ use App\Http\Controllers\LienKetTrangController;
 |
 */
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/trangchu', function () {
+    return view('home');
+})->middleware(['auth', 'verified'])->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
