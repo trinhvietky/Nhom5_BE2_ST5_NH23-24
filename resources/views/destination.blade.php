3e8c15b76f7cf88be4c18591885f1a8d035d7f2d
trinhvietky <161665737+trinhvietky@users.noreply.github.com>
2024-04-25 07:35:30 +0700

    Ky edit trang index

--- resources/views/destination.blade.php
+++ resources/views/destination.blade.php
@@ -30,30 +30,10 @@
             <h6 class="section-title bg-white text-center text-primary px-3">Destination</h6>
             <h1 class="mb-5">Popular Destination</h1>
         </div>
-        <div class="row g-3">
-            <div class="col-lg-7 col-md-6">
-                <div class="row g-3">
-                    @foreach($data_location->take(6) as $row)
-                    <div class="col-lg-6 col-md-12 wow zoomIn" data-wow-delay="0.1s" >
-                        <a class="position-relative d-block overflow-hidden" href="">
-                            <img class="img-fluid" style="width: 600px; height: 250px" src="{{ asset('img/'.$row->location_image) }}" alt="">
-                            <div class="bg-white text-danger fw-bold position-absolute top-0 start-0 m-3 py-1 px-2">{{$row->location_sale}}</div>
-                            <div class="bg-white text-primary fw-bold position-absolute bottom-0 end-0 m-3 py-1 px-2">{{$row->location_name}}</div>
-                        </a>
-                    </div>
-                    @endforeach
 
-                </div>
-            </div>
-            <!-- <div class="col-lg-5 col-md-6 wow zoomIn" data-wow-delay="0.7s" style="min-height: 350px;">
-                <a class="position-relative d-block h-100 overflow-hidden" href="">
-                    <img class="img-fluid position-absolute w-100 h-100" src="img/destination-4.jpg" alt="" style="object-fit: cover;">
-                    <div class="bg-white text-danger fw-bold position-absolute top-0 start-0 m-3 py-1 px-2">20% OFF</div>
-                    <div class="bg-white text-primary fw-bold position-absolute bottom-0 end-0 m-3 py-1 px-2">Indonesia</div>
-                </a>
-            </div> -->
-            @foreach($data_location->slice(6, 1) as $row)
-            <div class="col-lg-5 col-md-6 wow zoomIn" data-wow-delay="0.7s" >
+        <div class="row g-3">
+            @foreach($data_location as $row)
+            <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.1s">
                 <a class="position-relative d-block overflow-hidden" href="">
                     <img class="img-fluid" style="width: 600px; height: 250px" src="{{ asset('img/'.$row->location_image) }}" alt="">
                     <div class="bg-white text-danger fw-bold position-absolute top-0 start-0 m-3 py-1 px-2">{{$row->location_sale}}</div>
--- resources/views/package.blade.php
+++ resources/views/package.blade.php
@@ -36,14 +36,15 @@
                 <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                     <div class="package-item">
                         <div class="overflow-hidden">
-                            <img class="img-fluid" style="width: 600px;" src="{{ asset('img/'.$row->tour_image) }}" alt="">
+                            <img class="img-fluid" style="width: 600px; height: 250px" src="{{ asset('img/'.$row->tour_image) }}" alt="">
                         </div>
                         <div class="d-flex border-bottom">
-                            <small class="flex-fill text-center border-end py-2"><i class="fa fa-map-marker-alt text-primary me-2"></i>{{ $row->tour_name}}</small>
+                            <!-- <small class="flex-fill text-center border-end py-2"><i class="fa fa-map-marker-alt text-primary me-2"></i>{{ $row->tour_name}}</small> -->
                             <small class="flex-fill text-center border-end py-2"><i class="fa fa-calendar-alt text-primary me-2"></i>{{ $row->start_day}} -> {{ $row->end_day}}</small>
-                            <small class="flex-fill text-center py-2"><i class="fa fa-wheelchair text-primary me-2"></i>{{$row->location->location_name}}</small>
+                            <small class="flex-fill text-center py-2"><i class="fa fa-map-marker-alt text-primary me-2"></i>{{$row->location->location_name}}</small>
                         </div>
-                        <div class="text-center p-4">
+                        <div class="text-center p-1">
+                        <div class=" text-sm text-primary fw-bold flex-fill text-center py-1" style="font-size: 30px;"></i>{{ $row->tour_name}}</div>
                             <h3 class="mb-0">{{ $row->price}}$</h3>
                             <div class="mb-3">
                                 <small class="fa fa-star text-primary"></small>
@@ -52,7 +53,7 @@
                                 <small class="fa fa-star text-primary"></small>
                                 <small class="fa fa-star text-primary"></small>
                             </div>
-                            <p>Nằm trên ngọn đồi với khung cảnh tuyệt đẹp, {{ $row->tour_name}} là điểm đến nổi tiếng ở {{$row->location->location_name}} với những kiến trúc độc đáo, hệ thống cáp treo hàng đầu thế giới và các hoạt động giải trí phong phú cho du khách mọi lứa tuổi.</p>
+                            <p style="height: 150px;">Nằm trên ngọn đồi với khung cảnh tuyệt đẹp, {{ $row->tour_name}} là điểm đến nổi tiếng ở {{$row->location->location_name}} với những kiến trúc độc đáo, hệ thống cáp treo hàng đầu thế giới và các hoạt động giải trí phong phú cho du khách mọi lứa tuổi.</p>
                             <div class="d-flex justify-content-center mb-2">
                                 <a href="#" class="btn btn-sm btn-primary px-3 border-end" style="border-radius: 30px 0 0 30px;">Read More</a>
                                 <a href="#" class="btn btn-sm btn-primary px-3" style="border-radius: 0 30px 30px 0;">Book Now</a>
