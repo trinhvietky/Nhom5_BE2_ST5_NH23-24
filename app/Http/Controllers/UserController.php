ebe6c35c21eb9476c80a52d5164fa27b44289b71
trinhvietky <161665737+trinhvietky@users.noreply.github.com>
2024-05-09 07:28:07 +0700

    Ky edit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,7 @@ use App\Models\Guide;
 use App\Models\Location;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Models\Client;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
@@ -27,13 +28,15 @@ class UserController extends Controller
                 $tours = Tour::orderBy('tour_id')->get();
                 $guide = Guide::orderBy('guide_Id')->get();
                 $location = Location::orderBy('location_id')->get();
-                return view('user/home', ['data' => $tours, 'data_guide' => $guide, 'data_location' => $location]);
+                $client = Client::orderBy('client_id')->get();
+                return view('user/home', ['data' => $tours, 'data_guide' => $guide, 'data_location' => $location, 'data_comment' => $client]);
             }
             else if($userType == 'admin') {
                 $tours = Tour::orderBy('tour_id')->get();
                 $guide = Guide::orderBy('guide_Id')->get();
                 $location = Location::orderBy('location_id')->get();
-                return view('admin/home', ['data' => $tours, 'data_guide' => $guide, 'data_location' => $location]);
+                $client = Client::orderBy('client_id')->get();
+                return view('admin/home', ['data' => $tours, 'data_guide' => $guide, 'data_location' => $location, 'data_comment' => $client]);
             }
         }
     }
