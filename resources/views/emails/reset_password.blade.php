077643d3a306615367df4d944f7523b91c4402b6
maitrananhtuan <maituan5540@gmail.com>
2024-05-11 09:24:52 +0700

    Reset password

---
+++ resources/views/emails/reset_password.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Reset</title>
+</head>
+<body>
+    <p>Your password reset OTP is: <strong>{{ $otp }}</strong></p>
+    <p>Please use this OTP within 10 minutes to reset your password.</p>
+</body>
+</html>
