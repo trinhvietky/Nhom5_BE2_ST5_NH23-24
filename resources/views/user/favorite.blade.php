057ba1c21feb0cfdc59e456447c7d2ee2c3caed1
trinhvietky <161665737+trinhvietky@users.noreply.github.com>
2024-05-19 16:37:05 +0700

    css tour yeu thich

--- resources/views/user/favorite.blade.php
+++ resources/views/user/favorite.blade.php
@@ -38,27 +38,50 @@
                         <img class="img-fluid" style="width: 600px; height: 250px" src="{{asset('img/' . $favoriteTour->tour->tour_image)}}" alt="">
                         <div class="bg-white text-danger fw-bold position-absolute top-0 start-0 m-3 py-1 px-2">{{$favoriteTour->tour->tour_sale}}</div>
                     </div>
+
                     <div class="d-flex border-bottom" style="height: 50px;">
                         <small class="flex-fill text-center border-end py-2"><i class="fa fa-calendar-alt text-primary me-2"></i>{{ $favoriteTour->tour->start_day}}</small>
                         <small class="flex-fill text-center border-end py-2"><i class="fa fa-clock text-primary me-2"></i>{{ $favoriteTour->tour->time}}</small>
                         <small class="flex-fill text-center py-2"><i class="fa fa-plane-departure text-primary me-2"></i>{{$favoriteTour->tour->star_from}}</small>
                     </div>
-                    <div class="text-center p-4">
-                        <!-- Tên tour -->
-                        <h5 class="mb-0">{{$favoriteTour->tour->tour_name}}</h5>
-                    </div>
-                    <div class="d-flex border">
-                        <!-- Số điện thoại hướng dẫn viên -->
-                        <small class="flex-fill text-center border-end py-2"><i class="fa fa-phone text-primary me-2"></i>{{ $favoriteTour->tour->guide->guide_Pno}}</small>
-                        <!-- Email hướng dẫn viên -->
-                        <small class="flex-fill text-center border-end py-2"><i class="fa fa-envelope text-primary me-2"></i>{{ $favoriteTour->tour->guide->guide_Mail}}</small>
-                    </div>
-                    <div class="text-center">
-                        <!-- Mô tả tour -->
-                        <p>{{$favoriteTour->tour->tour_description}}</p>
-                        <!-- Liên kết đến trang chi tiết tour -->
-                        <a href="{{ route('user.tour.readmore', $favoriteTour->tour->tour_id) }}" class="btn btn-sm btn-primary px-3 border-end" style="border-radius: 30px 0 0 30px;">Đặt ngay</a>
-                        
+                    <h4 class=" text-primary fw-bold flex-fill text-center py-2" style="height: 50px;">{{ $favoriteTour->tour->tour_name}}</h4>
+                    <div class="text-center pt-2">
+
+                        <h5 class="mb-0 mt-3 text-danger">{{number_format($favoriteTour->tour->price, 0, ',', '.')}} vnđ</h5>
+
+                        <div class="mb-3">
+                            <small class="fa fa-star text-primary"></small>
+                            <small class="fa fa-star text-primary"></small>
+                            <small class="fa fa-star text-primary"></small>
+                            <small class="fa fa-star text-primary"></small>
+                            <small class="fa fa-star text-primary"></small>
+                        </div>
+                        <?php
+                        $tourDescription = $favoriteTour->tour->tour_description;
+
+                        // Chia chuỗi thành mảng các từ
+                        $words = explode(' ', $tourDescription);
+
+                        // Lấy 100 từ đầu tiên
+                        $mota = implode(' ', array_slice($words, 0, 50));
+                        ?>
+                        <p style="height: 130px;">{{$mota}} ... </p>
+
+                        <p class="text-danger" style="font-size: 20px; font-weight: bold;">Số chỗ còn trống: {{$favoriteTour->tour->total_seats - $favoriteTour->tour->booked_seats}} chỗ</p>
+
+                        <div class="d-flex justify-content-center mb-2 pb-2">
+                            <a href="{{ route('user.tour.readmore', $favoriteTour->tour->tour_id) }}" class="btn btn-sm btn-primary px-3 border-end" style="border-radius: 30px 0 0 30px;">Xem thêm</a>
+                            <a href="{{ route('user.tour.readmore', $favoriteTour->tour->tour_id) }}" class="btn btn-sm btn-primary px-3 border-end" style="border-radius: 0 0 0 0;">Đặt ngay</a>
+                            <form class="favorite-form" action="{{ route('favorite.add') }}" method="POST">
+                                @csrf
+                                <input type="hidden" name="tour_id" value="{{ $favoriteTour->tour->tour_id }}">
+                                <button type="submit" class="btn btn-sm btn-primary px-3 favorite-btn" style="border-radius: 0 30px 30px 0;">
+                                    <i class="far fa-heart heart-icon favorite-icon text-white"></i>
+                                </button>
+                            </form>
+
+
+                        </div>
                     </div>
                 </div>
             </div>
